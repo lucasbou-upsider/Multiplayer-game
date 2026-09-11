e7a7ffc4779a51bf3265ae06c9aaae6fd5bfc7d1
lucasbou <lucasbou2010@gmail.com>
2026-09-11 17:38:53 +0200

    test number beaucoup

--- player_controller.gd
+++ player_controller.gd
@@ -7,6 +7,11 @@ extends CharacterBody2D
 
 const SPEED := 500.0
 
+func _ready() -> void:
+	print("Joueur créé, path: ", get_path(), " sur peer id: ", multiplayer.get_unique_id())
+
+
+
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 	
@@ -17,19 +22,20 @@ func _enter_tree() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	look_at(get_global_mouse_position()) 
-	rotation += 90
-	
-	progress_bar.value = healt
-	
 	# First check if we have authority over this player
 	if not is_multiplayer_authority():
 		return
+
+	progress_bar.value = healt
+
+	look_at(get_global_mouse_position()) 
+	rotation += 90
 
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * SPEED
 
 	if Input.is_action_just_pressed("tire"):
 		request_bullet.rpc()
+		print("aaaa")
 	if Input.is_action_just_pressed("ui_select"):
 		$Icon.scale = Vector2(500, 50)
 		
@@ -40,14 +46,16 @@ func _physics_process(delta: float) -> void:
 		hat.scale += Vector2.ONE * delta
 	if Input.is_key_pressed(KEY_S):
 		hat.scale -= Vector2.ONE * delta
+
 var bullet = preload("res://bullet.tscn")
 
-@rpc("any_peer", "reliable")
+
+@rpc("any_peer","call_local", "reliable")
 func request_bullet() -> void:
 	if not multiplayer.is_server():
 		return
 
+	print("azeaze")
 	var insbullet = bullet.instantiate()
 	insbullet.position = global_position
-
-	get_parent().add_child(bullet, true)
+	get_parent().add_child(insbullet, true)
